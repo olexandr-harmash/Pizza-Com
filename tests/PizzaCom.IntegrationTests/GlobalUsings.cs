c277670a00341d035bc0ec6ddaf4493580c8fac4
olexandr-harmash <olexandr.harmash@gmail.com>
2024-07-30 06:28:01 -0700

    fix blueprint repository

---
+++ tests/PizzaCom.IntegrationTests/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using PizzaCom.Infrastructure;
+global using Microsoft.EntityFrameworkCore;
+global using PizzaCom.Domain.AggregatesModel;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
